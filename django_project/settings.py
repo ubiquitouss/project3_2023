c7f1b2110c555c61f65549c4fdc730d813defcf3
ubiquitouss <ubiquitouss@yahoo.com>
2023-12-03 03:01:08 +0000

    chapter 7 completed

--- django_project/settings.py
+++ django_project/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog.apps.BlogConfig',
+    'accounts.apps.AccountsConfig',
 ]
 
 MIDDLEWARE = [
@@ -123,3 +124,7 @@ STATICFILES_DIRS = [BASE_DIR / "static"]
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#user account
+LOGIN_REDIRECT_URL = "home" # for redirecting a user after login
+LOGOUT_REDIRECT_URL = "home" # for redirecting a user after logout
--- django_project/urls.py
+++ django_project/urls.py
@@ -4,5 +4,7 @@ from django.urls import path, include # new
 
 urlpatterns = [
  path("admin/", admin.site.urls),
+ path("accounts/", include("django.contrib.auth.urls")), #specially for creating accounts provided by django
+ path("accounts/", include("accounts.urls")),
  path("", include("blog.urls")), # new
 ]
